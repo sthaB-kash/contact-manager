7d131f079782b15dde22d71daa1b7babc1873031
Bikash Shrestha <shresthabikash2073@gmail.com>
2024-09-25 23:05:33 +0545

    feat: create contact model

---
+++ db/migrate/20240925163024_create_contacts.rb
@@ -0,0 +1,12 @@
+class CreateContacts < ActiveRecord::Migration[7.1]
+  def change
+    create_table :contacts do |t|
+      t.string :name
+      t.string :email
+      t.string :phone_number
+      t.string :address
+
+      t.timestamps
+    end
+  end
+end
