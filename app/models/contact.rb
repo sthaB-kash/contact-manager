eefb4fc8686ecd4a9991366e6ea0dbc935e09fd7
Bikash Shrestha <shresthabikash2073@gmail.com>
2024-09-25 23:49:12 +0545

    feat: add validation for name, email and phone number

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,8 @@
 class Contact < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_PHONE_REGEX = /\A[0-9]{10,13}\z/
+
+  validates :name, presence: true, length: { minimum: 2, maximum: 50 }
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
+  validates :phone_number, presence: true, format: { with: VALID_PHONE_REGEX }
 end
